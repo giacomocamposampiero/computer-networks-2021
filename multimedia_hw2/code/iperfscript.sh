c4337b7920120f8391dc83720fde1f904e5c52a8
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2021-05-16 10:53:41 +0200

    Experiment concluded

--- multimedia_hw2/code/iperfscript.sh
+++ multimedia_hw2/code/iperfscript.sh
@@ -1,9 +1,5 @@
-echo "date,c_IP,c_port,s_ip,s_port,trans_ID,time,trans,bitrate" > iperf.csv
-i=0
-while [ $i -le 439 ]
+echo "date,c_IP,c_port,s_ip,s_port,trans_ID,time,trans,bitrate" > iperftime.csv
+for i in  0.1 0.3 0.5 0.8 1 1.5 2 3 5 8 10 20 30 40 50 60
 do
-  iperf -c 88.80.187.84 -p 20180 -b 500M -N -y c >> iperf.csv
-  i=$(( $i + 1 ))
-  echo "$i"
-  sleep 30s
+  iperf -c 88.80.187.84 -p 20180 -b 100M -N -y c -t $i >> iperftime.csv
 done
