84a2688b87405446643fae88ca4dbf3c49b956ec
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2021-06-12 13:46:57 +0200

    Stable VAD algorithm implemented

---
+++ multimedia_final/ReadMe.txt
@@ -0,0 +1,10 @@
+# Voice Activity Detection
+
+## How to run the script
+In order to execute the script, open vad.m file with Matlab and run it (if necessary change Matlab path to the
+current folder). The script will automatically load data from every file in the "data/" folder whose name is in
+the format "inputaudio*.data". Then, the corresponding results will be saved in the "output/" folder in a file
+named "outputVAD*.txt", where * is the corresponding substring of the input file name.
+
+## Linux command to run the script
+matlab -nodisplay -r vad.m
--- multimedia_final/vad.m
+++ multimedia_final/vad.m
@@ -4,60 +4,144 @@
 % defined syntax 'inputaudioN.data'
 files = struct2cell(dir('data/inputaudio*.data'));
 
-% for each file contained in the directory
+% for each audio data file contained in the directory
 for i = 1:size(files,2)
 
     % get file name
     name = files{1,i};
+    % get input number
+    number = extractBetween(name, "inputaudio", ".data");
     % load track data
-    fileID = fopen(strcat('data/',name), 'r'); 
-    data = uint8(fread(fileID));
+    inid = fopen(strcat('data/',name), 'r'); 
+    data = fread(inid, 'int8');
+    fclose(inid);
+       
+    % number of packet of the track, floor of the division data/packet_size
+    pck_num = fix(size(data, 1)/160);
+    pck_dim = 160;
+    % classification values for each packet (0:INACTIVE, 1:ACTIVE)
+    output = 48.*ones(pck_num, 1);
+    % learning rate for adapting thresholds
+    p = 0.15;
     
-    % DEBUG
-    % save input track as .wav file that can be listened
-    audiowrite(strcat('output', name(11:11), '.wav'), data, 8000);
+    % classifiers output
+    class = zeros(3,1);
     
-    % number of packet of the track, floor of the division data/packet size
-    pck_num = fix(size(data, 1)/180)-1;
+    % simulate the flow of sound data; packets are considered in fixed size
+    % windows of 3 packets; since every packet contains 20ms of sound
+    % information, this will generate a total delay of 40ms, not
+    % comprehensive of computation and network time
     
-    output = energy_vad(data);
-    
-    % DEBUG
-    % apply the filter to the original track and save it as a .wav file, to
-    % make possible a comparison between the two tracks
-    for k = 0:pck_num-1
-        data(1+180*k : 180*(k+1)) = data(1+180*k : 180*(k+1)).*output(1,k+1);
+    % working on the assumption that the first 200ms of the track do not
+    % contain human voice samples, compute thresholds and other parameters
+    % used by some of the methods implemented for INACTIVE frames
+    ns_energy = 0;
+    f_thr = 200;
+    for ii = 1:9
+        wd = data(1+(ii-1)*pck_dim:(ii+2)*pck_dim);
+        ns_energy = ns_energy + 0.1*sum(wd.^2)/(pck_dim*3);
     end
-    audiowrite(strcat('outputmod', name(11:11), '.wav'), data, 8000);
+    
+    % flow of sound data, from packet 10 to the end of the flow
+    % packets are analyzed in groups of 3 with step of 1 packet; with this
+    % method, packets are never considered as single unit and this may help
+    % classification
+    for ii = 10:(pck_num-2)
+        % get the corresponding window frames
+        wd = data(1+(ii-1)*pck_dim:(ii+2)*pck_dim);
+        % ENERGY CLASSIFICATION
+        [class(1), ns_energy] = frame_energy_vad(wd, ns_energy, 5, p);
+        % ZERO CROSSINGS CLASSIFICATION
+        class(2) = frame_zc_vad(wd);
+        % FREQUENCY CLASSIFICATION
+        [class(3), f_thr] = frame_freq_vad(wd, f_thr, 3, p);   
+        if(any(class))
+            % at least one classificator classificated the frame as ACTIVE
+            % set an ACTIVE classification for the frame
+            output(ii) = 49;
+        end
+    end
+    
+    % open output file 
+    outname = ['output/outputVAD' number{1} '.txt'];
+    outid = fopen(outname, 'w+');
+    % write results to the output text file, as ASCII characters
+    fwrite(outid, output, 'uint8');
+    fclose(outid);
+        
+% 	% DEBUG
+%     % apply the filter to the original track and save it as a .wav file, to
+%     % make possible a comparison between the two tracks
+%     dec = zeros(size(data, 1),1);
+%     for ii = 0:pck_num-1
+%         for iii = 1:160
+%             dec(1+160*ii+iii) = data(1+160*ii+iii).*(output(ii+1, 1)-48);
+%         end
+%     end
+%     % listen to original and modified tracks
+%     player = audioplayer(data, 8000);
+%     play(player);
+%     disp("stop");
+%     player = audioplayer(dec, 8000);
+%     play(player);
+%     audiowrite(strcat('out', int2str(i), '.wav'), dec, 8000);
 end
 
-function [output] = energy_vad(data, pck_num)
-    % output classification for each input packet 
-    % 0 --> speech packet, 1 --> non-speech packet
-    output = zeros(1,pck_num);
-    % mean non-speech energy
-    ns_energy = 0;
-    
-    % working on the assumption that the first 200ms of the track does not
-    % contain human voice samples, compute the mean non-speech frame energy
-    % as mean of the first 10 packets
-    for k = 0:9
-        pc = double(data(1+180*k : 180*(k+1)));
-        ns_energy = ns_energy + sum(pc.^2) / 180;
+% close all files and clear the environment
+fclose('all');
+clear;
+
+function [classification, updtd] = frame_energy_vad(frame, ns_energy, k, p)
+    % compute frame energy
+    energy = sum(frame.^2)/size(frame,1);
+    if(energy > k*ns_energy)
+        % if energy is higher than INACTIVE threshold energy
+        % classify the frame as an ACTIVE frame
+        classification = 1;
+        % set delta INACTIVE frame energy to zero
+        updtd = ns_energy;
+    else
+        % if energy is lower than INACTIVE threshold energy
+        % classify the frame as an INACTIVE frame
+        classification = 0;
+        % compute delta INACTIVE frame energy that has to be added to
+        % initial ns_energy
+        updtd = ns_energy*(1-p) + energy*p;
     end
-    ns_energy = ns_energy / 10;
-    
-    % classify each other packet of the stream
-    for packet = 10:pck_num
-        bytes = double(data(1+packet*180:(packet+1)*180));
-        pck_energy = sum(bytes.^2)/180;
-        if(pck_energy > 1.3*ns_energy)
-            % packet energy is bigger than two times the mean non-speech
-            % energy, classify it as a speech packet
-            output(1, packet) = 1;
-        else
-            % non-speech packet, update mean ns energy
-            ns_energy = 0.8*ns_energy + 0.2*pck_energy;
-        end
+end
+
+function classification = frame_zc_vad(frame)
+    classification = 0;
+    crosses = 0;
+    % compute the number of zero crossings in the frame
+    for i = 2:size(frame)
+        crosses = crosses + abs(sign(frame(i))-sign(frame(i-1))); 
+    end
+    % the number of zero crossings usually lies in a fixed range, 
+    % between 15 and 50 for a 30ms sound frame, when the frame
+    % correspond to a voice frame; on the other hand, the number is
+    % randomic when the frame is a noise frame
+    % we decided to expand a little bit the limits of the range as missing
+    % a voice frame is considered generally more dangerous than including a
+    % false ACTIVE classification
+    if(crosses >= 10 && crosses <= 70)
+        classification = 1;
+    end  
+end
+
+function [classification, updtd] = frame_freq_vad(frame, threshold, k, p)
+    % default classification value to 0
+    classification = 0;
+    % get power for the frame frequency spectrum
+    pw = pspectrum(frame);
+    % compute the integral of power for frequencies in the range 1-1kHz
+    intg = sum(pw(1:500));
+    if(intg > k*threshold)
+        % ACTIVE frame, set corresponding output
+        classification = 1;
+        updtd = threshold;
+    else
+        % INACTIVE frame, update value of threshold for classification 
+        updtd = threshold*(1-p) + intg*p;
     end
 end
